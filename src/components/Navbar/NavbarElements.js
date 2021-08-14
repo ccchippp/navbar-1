c49ef590efff567e7f223cd50fab186c3a846bfe
ccchippp <clempke90@gmail.com>
2021-08-14 10:35:30 -0400

    finished hamburger styling

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -25,3 +25,17 @@ export const NavLink = styled(Link)`
     }
 `
 
+export const Bars = styled(FaBars)`
+    display: none;
+    color: #fff;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-100%, 75%);
+        font-size: 1.8 rem;
+        cursor: pointer;
+    }
+`
