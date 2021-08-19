d2760eae6d20da03eaa7388a4ef69f49741045c6
ccchippp <clempke90@gmail.com>
2021-08-19 10:00:04 -0400

    home responds

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/index.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages'
 
 function App() {
   return (
   <Router>
       <Navbar/>
       <Switch>
-        <Route path='/' exact component='Home'/>
+        <Route path='/' exact component={Home}/>
       </Switch>
   </Router>
   );
