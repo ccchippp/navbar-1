5520ebb46e899712ef30e034da1f4c9efcc29c4e
ccchippp <clempke90@gmail.com>
2021-08-19 09:53:17 -0400

    imported route and switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/index.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
   <Router>
       <Navbar/>
+      <Switch>
+        <Route path='/' exact component='Home'/>
+      </Switch>
   </Router>
   );
 }
