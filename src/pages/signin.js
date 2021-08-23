ce1a93707db1e9b9b74ba772369f032b24025ae4
ccchippp <clempke90@gmail.com>
2021-08-23 12:51:46 -0400

    starting form for signup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,7 +3,10 @@ import React from 'react'
 const signin = () => {
     return (
         <div>
-            
+            <form>
+                <label htmlFor="fname" className="fname"></label>
+                <input type="text" id='fname' name='fname'/><br></br>
+            </form>
         </div>
     )
 }
