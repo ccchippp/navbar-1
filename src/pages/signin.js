00643c409c03f001f2b25c973c9d9c3121d08f8f
ccchippp <clempke90@gmail.com>
2021-08-24 22:30:43 -0400

    continuing form inputs

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,6 +6,8 @@ const signin = () => {
             <form>
                 <label htmlFor="fname" className="fname"></label>
                 <input type="text" id='fname' name='fname'/><br></br>
+                <label htmlFor="lname" className="lname"></label>
+                <input type="text" id='lname' name='lname'/><br></br>
             </form>
         </div>
     )
